9fa8e7eb4e21484b60c69aacec7bcd43b5366f3d
Atsushi Otsubo <m@rirex.net>
2018-12-28 21:47:36 +0900

    initial commit

---
+++ RealtimeStepCounter-SwiftSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealtimeStepCounter-SwiftSample
+//
+//  Created by Mercury on 2018/12/28.
+//  Copyright © 2018 Rirex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
